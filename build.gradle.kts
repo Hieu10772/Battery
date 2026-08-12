7f757d50c5e49ebc2fa820298e03e30dee5a1a56
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-12 17:24:58 +0700

    Update and rename build.gradle to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("fabric-loom") version "1.12-SNAPSHOT"
+    id("maven-publish")
+}
+
+version = project.property("mod_version") as String
+group = project.property("maven_group") as String
+
+base {
+    archivesName.set(project.property("archives_base_name") as String)
+}
+
+repositories {
+    mavenCentral()
+    maven { name = "Fabric"; url = uri("https://maven.fabricmc.net/") }
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${project.property("minecraft_version")}")
+    mappings("net.fabricmc:yarn:${project.property("yarn_mappings")}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_version")}")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(21)
+    options.encoding = "UTF-8"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+jar {
+    from("LICENSE") {
+        rename { "${it}_${project.base.archivesName.get()}" }
+    }
+}
